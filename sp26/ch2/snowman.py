9201e418881e863b2562432244dbb6e9aa35c57f
norkish <norkish@gmail.com>
2026-01-22 14:43:42 -0700

    Create snowman.py

---
+++ sp26/ch2/snowman.py
@@ -0,0 +1,18 @@
+total_area = 0.0
+
+# compute/add top square
+width = 2.0
+area = width * width
+total_area = total_area + area
+
+# compute/add middle square
+width = 3.0
+area = width * width
+total_area = total_area + area
+
+# compute/add bottom square
+width = 5.0
+area = width * width
+total_area = total_area + area
+
+print("Total area =", total_area)
