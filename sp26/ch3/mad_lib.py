66e0a1612a480e766682cbdeb4f0bf5d625ce723
norkish <norkish@gmail.com>
2026-01-27 14:26:03 -0700

    Create mad_lib.py

---
+++ sp26/ch3/mad_lib.py
@@ -0,0 +1,12 @@
+print("This is one line\n\nThis is another")
+
+print("\t\tThis line has two tabs at the beginning\n\n")
+
+noun1 = input("Gimme a noun: ")
+int1 = int(input("Gimme a number: "))
+
+print("A poor", noun1, "is mistreated at home but remains kind.\n"
+      "With a bit of magic, she attends a royal ball,\n"
+      "where the prince falls for her. She flees at midnight,\n"
+      "leaving behind", int1, "shoe(s). The prince finds her, and her\n"
+      "life changes overnight.", sep="")
