154c9f60e181916f1768c0b1dae350132ee773a4
norkish <norkish@gmail.com>
2026-02-03 14:16:18 -0700

    Create printing_square_numbers.py

---
+++ sp26/ch3/printing_square_numbers.py
@@ -0,0 +1,8 @@
+# square_numbers.py—Write a program that prints
+# A) the numbers 0 to 100 and
+# B) their squares.
+# Use a for loop with the range(…) function.
+
+for i in range(101):
+    print(str(i) + "\t" + str(i*i))
+  
