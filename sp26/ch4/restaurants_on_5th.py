d5e0ca32eeeddffdf66684ca0696479d0309c1d9
norkish <norkish@gmail.com>
2026-02-03 14:18:27 -0700

    Create restaurants_on_5th.py

---
+++ sp26/ch4/restaurants_on_5th.py
@@ -0,0 +1,10 @@
+# Live Coding Demo—Write a program that uses a for loop
+# to print
+
+# One of the restaurants on 5th ave is Goody's
+# One of the restaurants on 5th ave is Taco Bell
+# etc...
+
+for restaurant in ["Goody's", "Taco Bell"]:
+    print("One of the restaurants on 5th ave is", restaurant)
+  
