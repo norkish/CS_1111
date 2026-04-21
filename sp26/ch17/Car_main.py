dfd29acf5eb7bd968f5e9008f63668f6eeef309d
norkish <norkish@gmail.com>
2026-04-21 14:20:02 -0600

    Create Car_main.py

---
+++ sp26/ch17/Car_main.py
@@ -0,0 +1,12 @@
+from Car import Car
+
+my_car = Car("Toyota", "Prius", "hybrid")
+my_car.paint("green")
+my_car.paint("blue")
+
+your_car = Car("Ford", "Mustang", "gasoline")
+your_car.paint("black")
+
+print("My car's color is " + my_car.color)
+
+print("Hello, World!")
